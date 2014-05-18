232e3a60bdfe7cd0e8fe5f715bcdd14ca8836453
François Pluchino <francois.pluchino@sonatra.com>
2014-05-18 11:46:36 +0200

    Updates phpdoc

--- DefaultValue/Exception/UnexpectedTypeException.php
+++ DefaultValue/Exception/UnexpectedTypeException.php
@@ -18,6 +18,10 @@ namespace Sonatra\Bundle\DefaultValueBundle\DefaultValue\Exception;
  */
 class UnexpectedTypeException extends InvalidArgumentException
 {
+    /**
+     * @param object|string $value
+     * @param int           $expectedType
+     */
     public function __construct($value, $expectedType)
     {
         parent::__construct(sprintf('Expected argument of type "%s", "%s" given', $expectedType, is_object($value) ? get_class($value) : gettype($value)));
