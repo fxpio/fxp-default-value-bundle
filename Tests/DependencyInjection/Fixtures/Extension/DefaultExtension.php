e3581981809bf54197c529a377e1c5a4699c0eef
François Pluchino <francois.pluchino@sonatra.com>
2014-05-18 11:46:47 +0200

    Adds some tests

---
+++ Tests/DependencyInjection/Fixtures/Extension/DefaultExtension.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonatra\Bundle\BootstrapBundle\Block\Extension;
+
+use Sonatra\Bundle\DefaultValueBundle\DefaultValue\AbstractTypeExtension;
+use Sonatra\Bundle\DefaultValueBundle\DefaultValue\ObjectBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * Block Block Extension.
+ *
+ * @author François Pluchino <francois.pluchino@sonatra.com>
+ */
+class BlockExtension extends AbstractTypeExtension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildObject(ObjectBuilderInterface $builder, array $options)
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function finishObject(ObjectBuilderInterface $builder, array $options)
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'test' => null,
+        ));
+
+        $resolver->addAllowedTypes(array(
+            'test' => array('null', 'string'),
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getExtendedType()
+    {
+        return 'default';
+    }
+}
---
+++ Tests/DependencyInjection/SonatraDefaultValueExtensionTest.php
@@ -0,0 +1,97 @@
+<?php
+
+/*
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonatra\Bundle\DefaultValueBundle\Tests\DependencyInjection;
+
+use Sonatra\Bundle\DefaultValueBundle\DependencyInjection\SonatraDefaultValueExtension;
+use Sonatra\Bundle\DefaultValueBundle\SonatraDefaultValueBundle;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+
+/**
+ * Bundle Extension Tests.
+ *
+ * @author François Pluchino <francois.pluchino@sonatra.com>
+ */
+class SonatraDefaultValueExtensionTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCompileContainerWithExtension()
+    {
+        $container = $this->getContainer();
+        $this->assertTrue($container->hasDefinition('sonatra_default_value.extension'));
+        $this->assertTrue($container->hasDefinition('sonatra_default_value.registry'));
+        $this->assertTrue($container->hasDefinition('sonatra_default_value.resolved_type_factory'));
+    }
+
+    public function testCompileContainerWithoutExtension()
+    {
+        $container = $this->getContainer(true);
+        $this->assertFalse($container->hasDefinition('sonatra_default_value.extension'));
+        $this->assertFalse($container->hasDefinition('sonatra_default_value.registry'));
+        $this->assertFalse($container->hasDefinition('sonatra_default_value.resolved_type_factory'));
+    }
+
+    /**
+     * @expectedException \Symfony\Component\Config\Definition\Exception\InvalidConfigurationException
+     */
+    public function testLoadExtensionWithoutAlias()
+    {
+        $this->getContainer(false, 'container_exception');
+    }
+
+    public function testLoadDefaultExtensionWithAlias()
+    {
+        $container = $this->getContainer(false, 'container_extension');
+        $this->assertTrue($container->hasDefinition('test.sonatra_default_value.type.default'));
+    }
+
+    /**
+     * @expectedException \Symfony\Component\Config\Definition\Exception\InvalidConfigurationException
+     */
+    public function testLoadDefaultExtensionWithoutAlias()
+    {
+        $this->getContainer(false, 'container_extension_exception');
+    }
+
+    /**
+     * Gets the container.
+     *
+     * @param bool   $empty    Compile container without extension
+     * @param string $services The services definition
+     *
+     * @return ContainerBuilder
+     */
+    protected function getContainer($empty = false, $services = null)
+    {
+        $container = new ContainerBuilder();
+        $bundle = new SonatraDefaultValueBundle();
+        $bundle->build($container); // Attach all default factories
+
+        if (!$empty) {
+            $extension = new SonatraDefaultValueExtension();
+            $container->registerExtension($extension);
+            $config = array();
+            $extension->load(array($config), $container);
+        }
+
+        if (null !== $services) {
+            $load = new XmlFileLoader($container, new FileLocator(__DIR__.'/Fixtures/Resources/config'));
+            $load->load($services.'.xml');
+        }
+
+        $container->getCompilerPassConfig()->setOptimizationPasses(array());
+        $container->getCompilerPassConfig()->setRemovingPasses(array());
+        $container->compile();
+
+        return $container;
+    }
+}
