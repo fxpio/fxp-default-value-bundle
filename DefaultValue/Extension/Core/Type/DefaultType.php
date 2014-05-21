ae43028183c2a1526a6745576b7d325af48f65fd
François Pluchino <francois.pluchino@sonatra.com>
2014-05-21 12:01:46 +0200

    Fixes alignment of phpdoc

--- DefaultValue/Extension/Core/Type/DefaultType.php
+++ DefaultValue/Extension/Core/Type/DefaultType.php
@@ -2,12 +2,12 @@
 
 /*
  * This file is part of the Sonatra package.
-*
-* (c) François Pluchino <francois.pluchino@sonatra.com>
-*
-* For the full copyright and license information, please view the LICENSE
-* file that was distributed with this source code.
-*/
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace Sonatra\Bundle\DefaultValueBundle\DefaultValue\Extension\Core\Type;
 
