bcac59a9092afa0b700526da3270d6dc7aba6085
François Pluchino <francois.pluchino@sonatra.com>
2014-05-23 18:00:13 +0200

    Adds dependency injection extension tests

--- Tests/DefaultValue/PreloadedExtensionTest.php
+++ Tests/DefaultValue/PreloadedExtensionTest.php
@@ -11,7 +11,6 @@
 
 namespace Sonatra\Bundle\DefaultValueBundle\Tests\DefaultValue;
 
-use Sonatra\Bundle\DefaultValueBundle\DefaultValue\ObjectTypeExtensionInterface;
 use Sonatra\Bundle\DefaultValueBundle\DefaultValue\PreloadedExtension;
 use Sonatra\Bundle\DefaultValueBundle\Tests\DefaultValue\Fixtures\Extension\UserExtension;
 use Sonatra\Bundle\DefaultValueBundle\Tests\DefaultValue\Fixtures\Type\UserType;
@@ -19,13 +18,8 @@ use Sonatra\Bundle\DefaultValueBundle\Tests\DefaultValue\Fixtures\Type\UserType;
 /**
  * @author François Pluchino <francois.pluchino@sonatra.com>
  */
-class PreloadedExtensionTest extends \PHPUnit_Framework_TestCase
+class PreloadedExtensionTest extends AbstractBaseExtensionTest
 {
-    /**
-     * @var PreloadedExtension
-     */
-    protected $extension;
-
     protected function setUp()
     {
         $types = array(
@@ -36,49 +30,5 @@ class PreloadedExtensionTest extends \PHPUnit_Framework_TestCase
         );
 
         $this->extension = new PreloadedExtension($types, $extensions);
-    }
-
-    protected function tearDown()
-    {
-        $this->extension = null;
-    }
-
-    public function testHasType()
-    {
-        $this->assertTrue($this->extension->hasType('Sonatra\Bundle\DefaultValueBundle\Tests\DefaultValue\Fixtures\Object\User'));
-        $this->assertFalse($this->extension->hasType('Foo'));
-    }
-
-    public function testHasTypeExtension()
-    {
-        $this->assertTrue($this->extension->hasTypeExtensions('Sonatra\Bundle\DefaultValueBundle\Tests\DefaultValue\Fixtures\Object\User'));
-        $this->assertFalse($this->extension->hasTypeExtensions('Foo'));
-    }
-
-    public function testGetType()
-    {
-        $type = $this->extension->getType('Sonatra\Bundle\DefaultValueBundle\Tests\DefaultValue\Fixtures\Object\User');
-
-        $this->assertInstanceOf('Sonatra\Bundle\DefaultValueBundle\DefaultValue\ObjectTypeInterface', $type);
-        $this->assertEquals('Sonatra\Bundle\DefaultValueBundle\Tests\DefaultValue\Fixtures\Object\User', $type->getClass());
-    }
-
-    public function testGetUnexistingType()
-    {
-        $this->setExpectedException('Sonatra\Bundle\DefaultValueBundle\DefaultValue\Exception\InvalidArgumentException');
-        $this->extension->getType('Foo');
-    }
-
-    public function testGetTypeExtension()
-    {
-        $exts = $this->extension->getTypeExtensions('Sonatra\Bundle\DefaultValueBundle\Tests\DefaultValue\Fixtures\Object\User');
-
-        $this->assertTrue(is_array($exts));
-        $this->assertCount(1, $exts);
-
-        /* @var ObjectTypeExtensionInterface $ext */
-        $ext = $exts[0];
-        $this->assertInstanceOf('Sonatra\Bundle\DefaultValueBundle\DefaultValue\ObjectTypeExtensionInterface', $ext);
-        $this->assertEquals('Sonatra\Bundle\DefaultValueBundle\Tests\DefaultValue\Fixtures\Object\User', $ext->getExtendedType());
     }
 }
